84dc9165427f6ea39db642d4670b82ee88abffb9
JimB99 <buismanjim@gmail.com>
2023-11-29 11:14:39 +0100

    Added more tests

--- test/test_NOrbit.py
+++ test/test_NOrbit.py
@@ -1,16 +1,56 @@
 import sys
-path = "/home/informatik2020/NOrbit/NOrbit"
+path = "NOrbit"
 sys.path.append(path)
 
 import numpy as np
-from NOrbit import kepToCart
+import NOrbit
+import pytest
+
 
 def test_kepToCart():
-    planet_elements = [np.array((1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))]
-    M_star = 1.0
-    pos = kepToCart(planet_elements[0], M_star)[0]
-    vel = kepToCart(planet_elements[0], M_star)[1]
-    #result = np.array([pos, vel])
-    result = np.round(vel[1], 4)
-    #assert result == np.array([[1.0, 0.0, 0.0], [0.0 , 0.0172021, 0.0]])
-    assert result == 0.0172
+    planet_elements = np.array((1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1e-5))
+    mass_star = 1.0
+
+    result = NOrbit.kepToCart(planet_elements, mass_star)
+    pos = np.array((1., 0., 0.))
+    vel = np.array((0., 0.017, 0.))
+
+    assert pytest.approx(result[0], 0.1) == pos
+    assert pytest.approx(result[1], 0.1) == vel
+
+def test_cartToKep():
+    positions = np.array((1.0, 1.0, 0.0))
+    velocities = np.array((0.1, 0.1, 0.0))
+    mass_planet = 1e-5
+    mass_star = 1.0
+
+    result = NOrbit.cartToKep(positions, velocities, mass_star, mass_planet)
+    assert pytest.approx(result, 0.1) == (0.0, 1.0, 0.0, 225.0, 0, 180.0, 1e-05)
+
+def test_calculate_acceleration():
+    positions = np.array([1.0, 1.0, 0.0], [0.0, 0.0, 0.0])
+    masses = np.array([1e-5], [1.0])
+
+    result = NOrbit.calculate_acceleration(positions, masses)
+    #assert
+    print(result)
+
+test_calculate_acceleration()
+
+'''
+def test_calculate_derivatives():
+
+    assert
+
+def test_move_to_barycenter():
+
+    assert
+
+def test_rk4_n_body():
+
+    assert
+
+def test_orbit():
+
+    assert
+'''
