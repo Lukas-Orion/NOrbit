b72a22138b84a0f9878da3f4a20416e019be3d66
JimB99 <101396309+JimB99@users.noreply.github.com>
2023-12-13 11:26:15 +0100

    Update test_NOrbit.py
    Added further tests

--- test/test_NOrbit.py
+++ test/test_NOrbit.py
@@ -28,14 +28,16 @@ def test_cartToKep():
     assert pytest.approx(result, 0.1) == (0.0, 1.0, 0.0, 225.0, 0, 180.0, 1e-05)
 
 def test_calculate_acceleration():
-    positions = np.array([1.0, 1.0, 0.0], [0.0, 0.0, 0.0])
-    masses = np.array([1e-5], [1.0])
+    positions = np.array([[ 4.24986205e-03, -2.16147702e-03, -7.22871314e-05],
+                        [-9.69141616e-01, -2.45313757e-01, -7.05363499e-05],
+                        [-4.45076172e+00,  2.26600558e+00,  7.57570172e-02]])
+    masses = np.array([ 1.000e+00, 3.039e-06, 9.542e-04])
 
     result = NOrbit.calculate_acceleration(positions, masses)
-    #assert
-    print(result)
+    assert pytest(result) == np.array([[ -1.09315857e-08,  4.90777799e-09,  1.71316619e-10],
+                                        [ 2.85191816e-04,  7.12527106e-05, -2.42462946e-10],
+                                        [ 1.05479855e-05, -5.37027351e-06, -1.79538757e-07]])
 
-test_calculate_acceleration()
 
 '''
 def test_calculate_derivatives():
